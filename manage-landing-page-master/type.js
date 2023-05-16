a4268d1b274ae809c8a4d14151ac19c9f5ebb2d1
XerXerS <darkxerxers29@gmail.com>
2023-05-16 14:09:50 +0100

    swiper js

--- manage-landing-page-master/type.js
+++ manage-landing-page-master/type.js
@@ -15,8 +15,21 @@ document.addEventListener("click", (event) => {
   });
 
 
+  var swiper = new Swiper(".slide-content", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
-
+ 
 
 
 
